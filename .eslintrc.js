f5ddeffbd71de724394a454f033b715a24648152
Gabriel Massadas <gmassadas@cloudflare.com>
2023-04-16 19:26:48 +0100

    Update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,12 @@ module.exports = {
   env: {
     node: true
   },
-  extends: [
+  'extends': [
     'plugin:vue/vue3-essential',
-    '@vue/standard'
+    'eslint:recommended'
   ],
   parserOptions: {
-    parser: 'babel-eslint'
+    ecmaVersion: 2020
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
